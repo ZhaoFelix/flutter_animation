4dc6c3d7a6a4a6955f3fecda2d58471be1c2bd01
zhaofelix <15021179915@163.com>
2021-07-19 17:53:21 +0800

    feat:项目初始化

---
+++ lib/01_text/main.dart
@@ -0,0 +1,22 @@
+/*
+ * @Author: Felix
+ * @Email: felix@qingmaoedu.com
+ * @Date: 2021-07-19 17:45:01
+ * @LastEditTime: 2021-07-19 17:48:41
+ * @FilePath: /flutter_animation/lib/01_text/main.dart
+ * Copyright © 2019 Shanghai Qingmao Network Technology Co.,Ltd All rights reserved.
+ */
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(StaticAimation());
+}
+
+class StaticAimation extends StatelessWidget {
+  const StaticAimation({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
