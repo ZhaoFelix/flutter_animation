24510fb369e0298407a2ff0c99e78f0d4e7ebb51
zhaofelix <15021179915@163.com>
2021-07-21 14:07:39 +0800

    fix:更新

---
+++ lib/dart/hello_dart_project/bin/hello_dart_project.dart
@@ -0,0 +1,18 @@
+/*
+ * @Author: Felix
+ * @Email: felix@qingmaoedu.com
+ * @Date: 2021-07-21 10:15:13
+ * @LastEditTime: 2021-07-21 13:58:18
+ * @FilePath: /flutter_animation/lib/dart/hello_dart_project/bin/hello_dart_project.dart
+ * Copyright © 2019 Shanghai Qingmao Network Technology Co.,Ltd All rights reserved.
+ */
+import 'dart:math';
+
+void main() {
+  print('Hello world!');
+  print(pi);
+  var salutation = 'Hello!';
+  // 获取字符串的unicode编码
+  print(salutation.codeUnits);
+  print(salutation);
+}
